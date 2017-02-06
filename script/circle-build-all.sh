55f40bcab0f0015f474e9e958bab63793497cfe4
eaglesakura <eagle.sakura@gmail.com>
2017-02-06 17:36:13 +0900

    circle.ymlを追加

---
+++ script/circle-build-all.sh
@@ -0,0 +1,34 @@
+#! /bin/sh -eu
+
+rm -rf ./ci-release
+mkdir ./ci-release
+
+echo "########################"
+echo "## Build for Linux"
+echo "########################"
+export GOOS=linux
+export GOARCH=amd64
+
+go build ./src/prjdep.go
+mkdir ./ci-release/linux
+mv ./prjdep ./ci-release/linux
+
+echo "########################"
+echo "## Build for Mac"
+echo "########################"
+export GOOS=darwin
+export GOARCH=amd64
+
+go build ./src/prjdep.go
+mkdir ./ci-release/mac
+mv ./prjdep ./ci-release/mac
+
+echo "########################"
+echo "## Build for Windows"
+echo "########################"
+export GOOS=windows
+export GOARCH=amd64
+
+go build ./src/prjdep.go
+mkdir ./ci-release/windows
+mv ./prjdep.exe ./ci-release/windows
