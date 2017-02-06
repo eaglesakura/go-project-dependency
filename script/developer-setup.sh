b776d15fda7c7fc0fb490ac23b838c705c6a4744
eaglesakura <eagle.sakura@gmail.com>
2017-02-06 18:42:59 +0900

    セットアップスクリプトを共通化

---
+++ script/developer-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -eu
+
+echo "############################"
+echo "### get libraries"
+echo "############################"
+echo "GOROOT=${GOROOT}"
+echo "GOPATH=${GOPATH}"
+
+prjdep restore
--- script/windows-intellij.bat
+++ script/windows-intellij.bat
@@ -5,4 +5,7 @@ CD /d %~dp0\..\
 SET GOPATH=%CD%\.gopath\windows
 SET PATH=%MINGW64_PATH%\bin;%PATH%
 
+echo GOROOT=%GOROOT%
+echo GOPATH=%GOPATH%
+
 %GO_IDEA_PATH%\bin\idea64.exe
